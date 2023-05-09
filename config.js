2f590bd67460eef40570d25b5cac08a1cca8a585
Nasir Mahmood Abbasi <nmabbasi@gmail.com>
2023-05-09 16:04:56 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,1 @@
+IDRViewer.config = {"pagecount":1,"title":"About_AGGA.html","author":"","fileName":"edited_About_AGGA_v8.pdf","bounds":[[935,1210]],"bookmarks":[],"thumbnailType":"jpg","pageType":"html","pageLabels":[]};
